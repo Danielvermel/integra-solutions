b3d477e16b0fc6c0bdbdb0a437e3fe99803d7a52
Daniel Leite <danielleite@Daniels-MacBook-Pro.local>
2023-08-13 21:34:06 +0100

    add pdf viewer boilerplate

---
+++ frontend/src/components/PdfViewerSideBySide.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pdf-viewer-container">
+    <div class="pdf-viewer">
+      Here
+      <vue-pdf-embed :source="pdfSrc" class="pdf-viewer-item" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import VuePdfEmbed from "vue-pdf-embed";
+
+const props = defineProps(["pdfSrc"]);
+console.log("- - - - - -- -- -- -- ");
+console.log(props.pdfSrc);
+</script>
+<style scoped>
+.pdf-viewer-container {
+  display: flex;
+  gap: 20px; /* Adjust the gap between the PDF viewers */
+}
+
+.pdf-viewer {
+  flex: 1;
+}
+
+.pdf-viewer-item {
+  width: 100%;
+  height: 500px; /* Adjust the height as needed */
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,44 @@
 <template>
   <body>
     <h1>Home Page</h1>
-    <input type="file" @change="analyzeFile" accept=".docx, .pdf" />
+
+    <PdfViewerSideBySide v-if="pdfOneSrc" :pdfSrc="pdfOneSrc" />
+    <input
+      type="file"
+      @change.prevent="selectFile($event)"
+      accept=".docx, .pdf"
+    />
   </body>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import PdfViewerSideBySide from "../components/PdfViewerSideBySide.vue";
 
-const analyzeFile = async (event) => {
-  const file = event.target.files[0]; // Get the first selected file
+const pdfOneSrc = ref(null); // Will hold the Blob URL of the selected PDF
+
+const selectFile = async (event) => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".docx, .pdf";
+  console.log("here - - ");
+
+  const file = event.target.files[0];
 
   if (!file) {
     console.error("No file selected");
     return;
   }
 
+  // Convert the selected file into a Blob URL
+  pdfOneSrc.value = URL.createObjectURL(file);
+  console.log(pdfOneSrc.value);
+
+  sendFileToServer(file);
+};
+
+const sendFileToServer = async (file) => {
   const path = "http://127.0.0.1:3000/analyze";
 
   const formData = new FormData();
@@ -29,9 +51,7 @@ const analyzeFile = async (event) => {
       headers: {
         "Content-Type": "multipart/form-data",
       },
-      xhrFields: {
-        withCredentials: true,
-      },
+      withCredentials: true,
     });
 
     console.log(response.data);
